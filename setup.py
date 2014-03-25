75e788dc66a1584498900bbe60175055f553987e
fiebiga <fiebig.adam@gmail.com>
2014-03-25 10:42:28 -0400

    Modified Version Number to 0.0.1

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ from setuptools.command.test import test as TestCommand
 import sys
 
 PROJECT = 'compysition'
-VERSION = '0.4.9'
+VERSION = '0.0.1'
 install_requires=['gevent>=1.0dev','argparse','greenlet>=0.3.2','jsonschema','prettytable','python-daemon',"pyyaml"]
 
 try:
