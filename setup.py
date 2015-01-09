17186d35763d31ab1203f8c35cdeee21d961a49c
fiebiga <fiebig.adam@gmail.com>
2015-01-09 10:55:35 -0500

    Incremented version

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ from setuptools.command.test import test as TestCommand
 import sys
 
 PROJECT = 'compysition'
-VERSION = '1.0.53'
+VERSION = '1.0.54'
 
 install_requires = ['gevent>=1.1',
                     'greenlet>=0.3.2',
