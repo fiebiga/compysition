28056baeefe383c2dedefd35d51f00e5e32dd914
fiebiga <fiebig.adam@gmail.com>
2015-04-21 16:23:15 -0400

    Version increment. Dropped dev versioning

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ from setuptools.command.test import test as TestCommand
 import sys
 
 PROJECT = 'compysition'
-VERSION = '1.1.01-dev'
+VERSION = '1.1.02'
 
 install_requires = ['gevent>=1.0',
                     'greenlet>=0.3.2',
