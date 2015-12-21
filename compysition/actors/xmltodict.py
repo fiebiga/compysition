37ba53594d44abccc8542a25785412024ff8e937
fiebiga <fiebig.adam@gmail.com>
2015-12-20 20:09:46 -0500

    Modified dicttoxml xmltodict to use xmltodict parse and unparse methods to promote better deterministic behavior between the two. The behavior of dicttoxml was different than xmltodict, so I wanted to use the same lib for both operations

--- compysition/actors/xmltodict.py
+++ compysition/actors/xmltodict.py
@@ -2,23 +2,34 @@ from __future__ import absolute_import
 
 from compysition.actor import Actor
 import xmltodict
+import json
 
 class XMLToDict(Actor):
     """
-    Actor implementation of the xmltodict lib
-        - Input: <XML>
-        - Output: <Dict>
+    *Actor implementation of the dicttoxml lib. Converts an incoming dictionary to XML*
+
+    Input/Output Types:
+        - Input: <(str) XML>
+        - Output: <(str) dict>
+
+    Parameters:
+        - name (REQ) (str)
+            | Actor Name
+
+        - escape_xml (bool) (Default: False)
+            | If set to True, a dict key or nested dict key that contains an XML-style string element will be
+            | XML escaped. If False, that XML will be appended to the XML element node created from the dictionary key
+            | as a literal XML tree
     """
 
     def __init__(self, name, *args, **kwargs):
-        self.xmltodict_kwargs = kwargs
         super(XMLToDict, self).__init__(name, *args, **kwargs)
 
     def consume(self, event, *args, **kwargs):
         try:
-            dict_data = xmltodict.parse(event.data, **self.xmltodict_kwargs)
+            dict_data = xmltodict.parse(event.data)
             if dict_data is not None:
-                event.data = dict_data
+                event.data = json.dumps(dict_data)
             else:
                 raise Exception("Incoming data was not valid XML")
         except Exception as err:
