42dfce28218ce5d819c1c783a32ae9b0784d17b1
fiebiga <fiebig.adam@gmail.com>
2016-03-15 06:17:31 -0400

    Added subclasses for HTTP response code modifier, and added support for key chains for modified values

--- compysition/actors/eventattributemodifier.py
+++ compysition/actors/eventattributemodifier.py
@@ -37,14 +37,16 @@ class EventAttributeModifier(Actor):
     Parameters:
 
         - name  (str):          The instance name.
-        - key   (str):          (Default: data) The key to set or update on the incoming event
-        - value (Anything):     (Default: {})   The value to assign to the key
-        - type  (static, xpath) (Default: static) Determines how the value is evaluated.
+        - key   (str):          (Default: "data") The key to set or update on the incoming event. Can be a key-chain
+                                    to access recursive dictionary elements using 'separator'
+        - value (Anything):     (Default: {}) The value to assign to the key
+        - separator (str)       (Default: "/") Delimiter for recursive key lookups
     '''
 
-    def __init__(self, name, key='data', value={}, log_change=False, *args, **kwargs):
+    def __init__(self, name, key='data', value={}, log_change=False, separator="/", *args, **kwargs):
         Actor.__init__(self, name, *args, **kwargs)
         self.value = value
+        self.separator = separator
         if key is None:
             self.key = name
         else:
@@ -58,12 +60,52 @@ class EventAttributeModifier(Actor):
         if self.log_change:
             self.logger.info("Changed event.{key} to {value}".format(key=self.key, value=modify_value), event=event)
 
-        event.set(self.key, modify_value)
-        self.send_event(event)
+        try:
+            event = self.get_key_chain_value(event, modify_value)
+            self.send_event(event)
+        except Exception as err:
+            self.send_error(event)
+
+    def get_key_chain_value(self, event, value):
+        #TODO: Redo this to not modify arrays
+        keys = self.key.split(self.separator)
+        event_key = keys.pop(0)
+        if len(keys) == 0:
+            event.set(event_key, value)
+        else:
+            current_step = event.get(event_key, None)
+            if not current_step:
+                current_step = {}
+                event.set(event_key, current_step)
+
+            try:
+                while True:
+                    try:
+                        item = keys.pop(0)
+                        if len(keys) > 0:
+                            next_step = current_step[item]
+                            current_step = next_step
+                        else:
+                            current_step[item] = value
+                    except KeyError:
+                        next_step = {} if len(keys) > 0 else value
+                        current_step[item] = next_step
+                        current_step = next_step
+                    except IndexError:
+                        break
+            except Exception as err:
+                self.logger.error("Unable to follow key chain. Ran into non-dict value of '{value}'".format(value=current_step), event=event)
+                raise
+
+        return event
 
     def get_modify_value(self, event):
         return self.value
 
+class HTTPStatusModifier(EventAttributeModifier):
+
+    def __init__(self, name, value="200 OK", *args, **kwargs):
+        super(HTTPStatusModifier, self).__init__(name, key="wsgi/status", value=value, *args, **kwargs)
 
 class XpathEventAttributeModifer(EventAttributeModifier):
 
@@ -97,6 +139,8 @@ class XpathEventAttributeModifer(EventAttributeModifier):
 
         return value
 
+class HTTPXpathEventAttributeModifier(XpathEventAttributeModifer, HTTPStatusModifier):
+    pass
 
 class JSONEventAttributeModifier(EventAttributeModifier):
 
@@ -118,3 +162,6 @@ class JSONEventAttributeModifier(EventAttributeModifier):
             value = None
 
         return value
+
+class HTTPJSONEventAttributeModifier(JSONEventAttributeModifier, HTTPStatusModifier):
+    pass
