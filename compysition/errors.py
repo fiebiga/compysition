06eb5fdbec10d6a6f75a24eb8daa62a8c4ff215e
fiebiga <fiebig.adam@gmail.com>
2016-05-09 14:18:29 -0400

    Added several exceptions to errors.py meant to represent possible programmatic states for each event

--- compysition/errors.py
+++ compysition/errors.py
@@ -24,53 +24,115 @@
 #
 #
 
+class CompysitionException(Exception):
 
-class QueueLocked(Exception):
-    pass
+    def __init__(self, message=""):
+        if not isinstance(message, list):
+            message = [message]
 
-class QueueEmpty(Exception):
-    def __init__(self, message):
-        Exception.__init__(self, message)
+        super(CompysitionException, self).__init__(message)
 
-class QueueFull(Exception):
-    def __init__(self, message):
-        Exception.__init__(self, message)
 
-class QueueMissing(Exception):
+class QueueEmpty(CompysitionException):
     pass
 
 
-class QueueOccupied(Exception):
+class QueueFull(CompysitionException):
     pass
 
 
-class QueueConnected(Exception):
+class QueueConnected(CompysitionException):
     pass
 
 
-class SetupError(Exception):
+class SetupError(CompysitionException):
     pass
 
 
-class ReservedName(Exception):
+class ReservedName(CompysitionException):
     pass
 
 
-class ModuleInitFailure(Exception):
+class ActorInitFailure(CompysitionException):
+    """**A specific Actor initialization failed**"""
     pass
 
 
-class NoSuchModule(Exception):
+class InvalidEventConversion(CompysitionException):
+    """**An attempt to convert Event or an Event subclass to a different class was impossible**"""
     pass
 
-class NoConnectedQueues(Exception):
+
+class InvalidEventDataModification(CompysitionException):
+    """**An attempt to modify Event.data to an invalid format for the Event subclass occurred**"""
     pass
 
-class InvalidInputException(Exception):
+
+class InvalidActorOutput(CompysitionException):
+    """**An Actor sent an event that was not defined as a valid output Event class**"""
     pass
 
-class InvalidEventDataException(Exception):
+
+class InvalidActorInput(CompysitionException):
+    """**An Actor received an event that was not defined as a valid input Event class**"""
     pass
 
-class InvalidModificationException(Exception):
+
+class ResourceNotModified(CompysitionException):
+    """**An Actor attempted to modify an external persistent entity, but that modification was not successful**"""
+    pass
+
+
+class MalformedEventData(CompysitionException):
+    """**Event data was malformed for a particular Actor to work on it**"""
+    pass
+
+
+class UnauthorizedEvent(CompysitionException):
+    """**Event either did not contain credentials for a restricted actor, or contained invalid credentials**"""
+    pass
+
+
+class ForbiddenEvent(CompysitionException):
+    """**Event was authenticated properly for a given Actor, but the credentials were not granted permissions for the requested action**"""
+    pass
+
+
+class ResourceNotFound(CompysitionException):
+    """**A specific queue was requested on an Actor, but that queue was not defined or connected for that actor**"""
+    pass
+
+
+class EventCommandNotAllowed(CompysitionException):
+    """**A semantic property on an incoming event that modified actor behavior was not implemented**"""
+    pass
+
+
+class ActorTimeout(CompysitionException):
+    """**An Actor timed out the requested work on an incoming event**"""
+    pass
+
+
+class ResourceConflict(CompysitionException):
+    """**An event attempted to draw upon a persistant storage resource that is no longer available**"""
+    pass
+
+
+class ResourceGone(CompysitionException):
+    """**An event attempted to draw upon a persistant storage resource that is no longer available**"""
+    pass
+
+
+class UnprocessableEventData(CompysitionException):
+    """**Event data was well formed, but unprocessable for application logic semantic purposes**"""
+    pass
+
+
+class EventRateExceeded(CompysitionException):
+    """**The allowed event rate for a given actor has been exceeded**"""
+    pass
+
+
+class ServiceUnavailable(CompysitionException):
+    """**A defined Event.service was requested but that particular service was not found**"""
     pass
