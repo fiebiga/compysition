89ae4783393b9dfd86887f0b969a6e55b25bdcde
Don Laffitte <don.laffitte@cuanswers.com>
2017-08-03 07:14:30 -0400

    [modify_event_data]: Update eventattributemodifier to have the ability to modify the event data specifically. Most commonly utilized (so far) to rebuild event data after a DB write

--- compysition/actors/eventattributemodifier.py
+++ compysition/actors/eventattributemodifier.py
@@ -178,3 +178,27 @@ class JSONEventAttributeModifier(EventAttributeModifier):
 
 class HTTPJSONAttributeModifier(JSONEventAttributeModifier, HTTPStatusModifier):
     pass
+
+#This will allow you to modify the event data to contain something that exists directly on the event.
+#IF your data is a list (like after the json db write), it's going to reinitialize it
+#as a dict. This will use self.key (what you called it with) to both get it from the event
+#and name the resulting data attribute.
+#
+#
+class EventDataEventAttributeModifier(EventAttributeModifier):
+
+    def get_modify_value(self, event):
+        value = getattr(event, self.key)
+        event.data = {self.key:value}
+        return value
+
+#This will allow you to modify the event data to contain any static value that you send.
+#IF your data is a list (like after the json db write), it's going to reinitialize it
+#as a dict.
+#
+#
+class EventDataStaticAttributeModifier(EventAttributeModifier):
+
+    def get_modify_value(self, event):
+        event.data = {self.key:self.value}
+        return self.value
