3bdd8d149364b5811651d630b8bd4d130c688fe6
drpoggi <dp4433@gmail.com>
2018-10-31 15:55:06 -0400

    Remove python 3.3 classifier from setup.y

--- setup.py
+++ setup.py
@@ -67,7 +67,6 @@ setup(
                  'Programming Language :: Python',
                  'Programming Language :: Python :: 2',
                  'Programming Language :: Python :: 2.7',
-                 'Programming Language :: Python :: 3.3',
                  'Programming Language :: Python :: 3.6',
                  'Intended Audience :: Developers',
                  'Intended Audience :: System Administrators'],
